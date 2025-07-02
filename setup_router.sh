d84f50ec7917556e0294ca98216edd7f45e7b882
Diego Rodrigues <diego.mrodrigues6@gmail.com>
2025-07-02 11:25:02 -0300

    Fix router setup path

--- setup_router.sh
+++ setup_router.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. replica/router.proto
+python -m grpc_tools.protoc -I database/replication \
+    --python_out=. --grpc_python_out=. \
+    database/replication/replica/router.proto
